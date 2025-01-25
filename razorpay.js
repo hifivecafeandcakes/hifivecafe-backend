34f5bb2eaf2e2bb289b03f8be2e77d0900505624
loganathNew <loguphp2020@gmail.com>
2025-01-25 13:07:48 +0530

    pay up

--- razorpay.js
+++ razorpay.js
@@ -5,8 +5,11 @@ dotenv.config()
 
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+    // key_id: process.env.RAZORPAY_KEY_ID,
+    // key_secret: process.env.RAZORPAY_KEY_SECRET,
+
+    key_id: "rzp_live_lKsHAKbB6toKJk",
+    key_secret: "CMHrDCdRoakvBrcndd4gd9NU",
 });
 
 
